71d9bf0758abbaf3e67dec97b3f4a35bf1277ac6
Gozzissimo <fantini.nico@gmail.com>
2022-06-20 14:41:20 +0200

    update 20-06-2022

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,16 +5,24 @@ using System.Diagnostics;
 namespace csharp_blog_frontend.Controllers
 {
     public class HomeController : Controller
-    {    
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
-
         [HttpGet]
         public IActionResult Details(int id)
         {
+            ViewData["id"] = id;
+
             return View();
         }
 
@@ -24,11 +32,32 @@ namespace csharp_blog_frontend.Controllers
             return View();
         }
 
-        [HttpPost]
+        public IActionResult CreateFile()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult Update(int id)
         {
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
